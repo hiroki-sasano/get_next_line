fe97a6c41927d559d156de8f167d3a37a1c716f8
Hiroki Sasano <hisasano@c2r3s8.42tokyo.jp>
2025-05-25 11:18:12 +0900

    tetuya

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hisasano <hisasano@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 20:40:01 by hisasano          #+#    #+#             */
-/*   Updated: 2025/05/24 19:52:41 by hisasano         ###   ########.fr       */
+/*   Updated: 2025/05/25 00:02:28 by hisasano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,42 +17,6 @@
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 4096
 #endif
-/*
-戻り値：
-成功時：読み取った1行の文字列（'\n'を含む）
-EOFまたはエラー時：NULL
-*/
-
-/*
-long pagesize = sysconf(_SC_PAGESIZE);   // 例: 4096 バイト
-size_t bufsize = pagesize * 8;           // 32 KiB
-
-char *buf = malloc(bufsize);
-ssize_t r = read(fd, buf, bufsize);
-*/
-
-/*
-ページサイズ確認コマンド
-getconf PAGESIZE
-# または
-pagesize
-*/
-/*
-Does your function still work if the BUFFER_SIZE value is 9999? If
-it is 1? 10000000? Do you know why?
-
-ulimit -s
-*/
-/*
-| フラグ        | 意味                    |
-| ---------- | --------------------- |
-| `O_RDONLY` | 読み取り専用で開く             |
-| `O_WRONLY` | 書き込み専用で開く             |
-| `O_RDWR`   | 読み書き両用で開く             |
-| `O_CREAT`  | ファイルが無ければ新規作成する       |
-| `O_TRUNC`  | ファイルを開いたときに中身を空にする    |
-| `O_APPEND` | 書き込むときに常に末尾に追加するようにする |
-*/
 
 size_t	my_strlen(const char *s)
 {
@@ -147,11 +111,13 @@ t_fddata	*get_or_make(t_fddata **head, int fd)
 			return cur;
 		cur = cur->next;
 	}
-	node = malloc(sizeof(t_fddata) * 1);
+	node = malloc(sizeof(*node));
 	if (!node)
 		return NULL;
 	node->fd = fd;
     node->buf = my_strdup("");
+	node->next = NULL;
+	node->prev = NULL;
 	lst_add_back(head, node);
     return node;
 }
@@ -208,9 +174,9 @@ char    *select_line(t_fddata *node)
 
     nl_p = my_strchr(node->buf, '\n');
     if (nl_p)
-        len = (nl_p - node->buf - 1);
+        len = (size_t)(nl_p - node->buf) + 1;
     else    
-        len = my_strlen(nl_p);
+        len = my_strlen(node->buf);
     new_line = malloc(sizeof(char) * (len + 1));
     if (!new_line)
         return NULL;
@@ -231,8 +197,8 @@ char *get_next_line(int fd)
     int b_r;
 	char *tmp;
 
-	head = NULL;
 	node = get_or_make(&head, fd);
+	b_r = 0;
 	if (!node)
 		return NULL;
     while (!my_strchr(node->buf, '\n') && (b_r = read(fd, buf, BUFFER_SIZE)) > 0)
@@ -250,75 +216,4 @@ char *get_next_line(int fd)
         return NULL;
     }
     return select_line(node);
-    //切り出し→残り格納
-
 }
-
-#include <fcntl.h>      // open
-#include <stdio.h>      // printf
-#include <stdlib.h>     // free
-#include "get_next_line.h"  // get_next_lineのプロトタイプ
-
-int main(void)
-{
-    int fd = open("test.txt", O_RDONLY);
-    if (fd == -1)
-        return (1);
-
-    char *line;
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("[line] %s", line);  // 改行は get_next_line が含む前提
-        free(line);
-    }
-
-    close(fd);
-    return 0;
-}
-
-// #include <fcntl.h>   // open
-// #include <stdio.h>   // printf
-// #include <stdlib.h>  // free
-// #include <unistd.h>  // close
-// #include "get_next_line.h"  // get_next_lineのプロトタイプ
-
-// int main(void)
-// {
-//     int fd1 = open("test1.txt", O_RDONLY);
-//     int fd2 = open("test2.txt", O_RDONLY);
-
-//     if (fd1 == -1 || fd2 == -1)
-//     {
-//         perror("open");
-//         return 1;
-//     }
-
-//     char *line1 = get_next_line(fd1);
-//     char *line2 = get_next_line(fd2);
-
-//     printf("file1: %s", line1);
-//     printf("file2: %s", line2);
-
-//     free(line1);
-//     free(line2);
-
-//     close(fd1);
-//     close(fd2);
-
-//     return 0;
-// }
-
-
-
-
-
-/*
-種類	read 可能	備考
-通常ファイル	✔️	open()で取得
-標準入力（キーボード）	✔️	fd = 0
-パイプ／FIFO	✔️	pipe() / mkfifo()
-ソケット	✔️	socket(), accept() 後の fd
-/dev/zero など	✔️	特殊デバイス（デバイスドライバに依存）
-/proc/cpuinfo	✔️	仮想ファイルシステム（procfs）
-*/
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,27 +6,33 @@
 /*   By: hisasano <hisasano@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 20:43:37 by hisasano          #+#    #+#             */
-/*   Updated: 2025/05/24 05:08:23 by hisasano         ###   ########.fr       */
+/*   Updated: 2025/05/25 00:02:40 by hisasano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stddef.h>
+# include <stddef.h>
+
+
 typedef struct s_fddata
 {
-	int             fd;
-	char            *buf;
-	struct s_fddata *prev;
-	struct s_fddata *next;
-}	t_fddata;
+	int				fd;
+	char			*buf;
+	struct s_fddata	*prev;
+	struct s_fddata	*next;
+}					t_fddata;
 
-
-char *get_next_line(int fd);
-void get_next_line_utils(t_fddata *fd_list);
-
-
-
+char				*get_next_line(int fd);
+static char			*select_line(t_fddata *node);
+static void			*my_memcpy(void *dst, const void *src, size_t n);
+static void			lst_remove(t_fddata **head, t_fddata *node);
+static char			*my_strchr(const char *s, int c);
+static t_fddata		*get_or_make(t_fddata **head, int fd);
+static void			lst_add_back(t_fddata **lst, t_fddata *new);
+static char			*my_strjoin(char const *s1, char *s2);
+static char			*my_strdup(const char *s);
+static size_t		my_strlen(const char *s);
 
 #endif
