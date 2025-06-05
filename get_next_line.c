d0a77fe861ce71b22eb54a453cc3b4d3b8cdad10
Hiroki Sasano <hisasano@c2r1s1.42tokyo.jp>
2025-06-05 21:50:21 +0900

    6_5_3

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hisasano <hisasano@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 20:40:01 by hisasano          #+#    #+#             */
-/*   Updated: 2025/06/04 20:32:21 by hisasano         ###   ########.fr       */
+/*   Updated: 2025/06/05 21:40:11 by hisasano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,35 @@
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 4096
 #endif
+
+static char	*select_line_or_free(t_fddata *node, size_t free_on)
+{
+	char	*new_line;
+	char	*nl_p;
+	char	*tmp;
+	size_t	len;
+
+	if (free_on)
+	{
+		free(node->buf);
+		free(node);
+		return (NULL);
+	}
+	nl_p = my_strchr(node->buf, '\n');
+	if (nl_p)
+		len = (size_t)(nl_p - node->buf) + 1;
+	else
+		len = my_strlen(node->buf);
+	new_line = malloc(sizeof(char) * (len + 1));
+	if (!new_line)
+		return (NULL);
+	my_memcpy(new_line, node->buf, len);
+	new_line[len] = '\0';
+	tmp = my_strdup(node->buf + len);
+	free(node->buf);
+	node->buf = tmp;
+	return (new_line);
+}
 
 static void	remove_or_add(t_fddata **lst, t_fddata *new, size_t rmv_on)
 {
@@ -32,8 +61,8 @@ static void	remove_or_add(t_fddata **lst, t_fddata *new, size_t rmv_on)
 			new->next->prev = new->prev;
 		if (*lst == new)
 			*lst = new->next;
-		free(new->buf);
-		free(new);
+		select_line_or_free(new, 1);
+		return ;
 	}
 	if (!*lst)
 		*lst = new;
@@ -70,43 +99,18 @@ static t_fddata	*get_or_make(t_fddata **head, int fd)
 	return (node);
 }
 
-static char	*select_line(t_fddata *node)
-{
-	char	*new_line;
-	char	*nl_p;
-	char	*tmp;
-	size_t	len;
-
-	nl_p = my_strchr(node->buf, '\n');
-	if (nl_p)
-		len = (size_t)(nl_p - node->buf) + 1;
-	else
-		len = my_strlen(node->buf);
-	new_line = malloc(sizeof(char) * (len + 1));
-	if (!new_line)
-		return (NULL);
-	my_memcpy(new_line, node->buf, len);
-	new_line[len] = '\0';
-	tmp = my_strdup(node->buf + len);
-	free(node->buf);
-	node->buf = tmp;
-	return (new_line);
-}
-
-static int	gnl_refill(t_fddata **head, t_fddata *node, int fd, char *buf)
+static int	gnl_refill(t_fddata *node, int fd, char *buf)
 {
 	int		br;
 	char	*tmp;
 
-	br = 1;
-	while (!my_strchr(node->buf, '\n') && br > 0)
+	while (!my_strchr(node->buf, '\n'))
 	{
 		br = read(fd, buf, BUFFER_SIZE);
-		if (br <= 0)
-		{
-			remove_or_add(head, node, 1);
+		if (br < 0)
 			return (-1);
-		}
+		if (br == 0)
+			return (0);
 		buf[br] = '\0';
 		tmp = node->buf;
 		node->buf = my_strjoin(tmp, buf);
@@ -114,7 +118,7 @@ static int	gnl_refill(t_fddata **head, t_fddata *node, int fd, char *buf)
 		if (!node->buf)
 			return (-1);
 	}
-	return (br);
+	return (1);
 }
 
 char	*get_next_line(int fd)
@@ -124,20 +128,23 @@ char	*get_next_line(int fd)
 	char			*buf;
 	int				br;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	buf = malloc(BUFFER_SIZE + 1);
 	if (!buf)
 		return (NULL);
 	node = get_or_make(&head, fd);
 	if (!node)
-		return (free(buf), NULL);
-	br = gnl_refill(&head, node, fd, buf);
-	free(buf);
-	if (br == -1)
+	{
+		free(buf);
 		return (NULL);
-	if (br == 0 && (node->buf == NULL || !*node->buf))
+	}
+	br = gnl_refill(node, fd, buf);
+	free(buf);
+	if ((br < 0) || (br == 0 && (node->buf == NULL || !*node->buf)))
 	{
 		remove_or_add(&head, node, 1);
 		return (NULL);
 	}
-	return (select_line(node));
+	return (select_line_or_free(node, 0));
 }
--- main_basic.c
+++ main_basic.c
@@ -5,57 +5,57 @@
 #include <string.h>     
 #include "get_next_line.h"    
 
-// void test_fd(int fd, const char *label)
+void test_fd(int fd, const char *label)
+{
+    char *line;
+    int line_num = 0;
+
+    printf("===== Testing: %s =====\n", label);
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("[%02d] %s\n", ++line_num, line);
+        free(line);
+    }
+    printf("===== End: %s =====\n\n", label);
+}
+
+int main(int argc, char **argv)
+{
+    // 1. ファイル読み取り (複数対応)
+    for (int i = 1; i < argc; i++)
+    {
+        int fd = open(argv[i], O_RDONLY);
+        if (fd == -1)
+        {
+            perror("open");
+            continue;
+        }
+        test_fd(fd, argv[i]);
+        close(fd);
+    }
+
+    // 2. 標準入力を手動テストしたいとき
+    if (argc == 1)
+    {
+        printf("標準入力からテスト（Ctrl+Cで終了）:\n");
+        test_fd(0, "stdin");
+    }
+
+    return 0;
+}
+
+// int main(void)
 // {
-//     char *line;
+// 	// int fd = open("test.txt", O_RDONLY);
+
+// 	char *line;
 //     int line_num = 0;
 
-//     printf("===== Testing: %s =====\n", label);
-//     while ((line = get_next_line(fd)) != NULL)
+//     line = get_next_line(42);
 //     {
 //         printf("[%02d] %s", ++line_num, line);
 //         free(line);
 //     }
-//     printf("===== End: %s =====\n\n", label);
+//     printf("===== End =====\n\n");
+
 // }
-
-// int main(int argc, char **argv)
-// {
-//     // 1. ファイル読み取り (複数対応)
-//     for (int i = 1; i < argc; i++)
-//     {
-//         int fd = open(argv[i], O_RDONLY);
-//         if (fd == -1)
-//         {
-//             perror("open");
-//             continue;
-//         }
-//         test_fd(fd, argv[i]);
-//         close(fd);
-//     }
-
-//     // 2. 標準入力を手動テストしたいとき
-//     if (argc == 1)
-//     {
-//         printf("標準入力からテスト（Ctrl+Cで終了）:\n");
-//         test_fd(0, "stdin");
-//     }
-
-//     return 0;
-// }
-
-int main(void)
-{
-	// int fd = open("test.txt", O_RDONLY);
-
-	char *line;
-    int line_num = 0;
-
-    line = get_next_line(42);
-    {
-        printf("[%02d] %s", ++line_num, line);
-        free(line);
-    }
-    printf("===== End =====\n\n");
-
-}
